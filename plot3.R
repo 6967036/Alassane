ef470b3b65624a1238afe0559df4a4b20e554b0a
6967036 <6967036@users.noreply.github.com>
2014-08-10 22:59:04 +0000

    plot3.R

---
+++ plot3.R
@@ -0,0 +1,26 @@
+#======================Plot3=====================================================
+base <- read.table("household_power_consumption.txt",header=TRUE, sep =";",
+                   quote = "", dec =".",
+                   col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+                   nrows = 2880,skip = 66636,fill=TRUE)
+
+x <- base[,"Sub_metering_1"]
+y <- base[,"Sub_metering_2"]
+z <- base[,"Sub_metering_3"]
+matrice <- c(x,y,z)
+mat <- matrix(matrice,nrow=2880)
+dim(mat)
+serie1 <- ts(x ,frequency=1440)
+serie2 <- ts(y ,frequency=1440)
+serie3 <- ts(z ,frequency=1440)
+
+matplot(seq(2880), mat, type = "l", lty = c("solid", "solid","solid"),
+        lwd =1, col = c("black","red", "blue"), xlab=" ", xaxt = "n",ylab="Energy sub metering")
+axis(1, 1:3, c("Thu","Fri","Sat"))
+
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"),
+       text.col = "black", lty = c(1, 1, 1), pch = c(-1, -1, -1),
+       merge = TRUE, bg = 'white')
+
+dev.copy(png, file="plot3.png")
+dev.off()
